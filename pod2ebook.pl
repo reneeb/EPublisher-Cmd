7137e5b99b55475bd2c0f07531d90862c5eeda59
Boris Däppen <boris_daeppen@bluewin.ch>
2012-02-20 17:04:41 +0100

    Added options and functionality

--- pod2ebook.pl
+++ pod2ebook.pl
@@ -9,46 +9,63 @@ my $out_file = 'eBook';
 my @in_files;
 
 my $help_msg = <<END;
-  Options:
-    -h              Display this help
-    -t [mobi|epub]  Define type of output, default is 'epub'
-    -o [filename]   Set filename for the resulting eBook, default is 'eBook'
+  Main Options:
+    -h             Display this help
+    -f [mobi|epub] Define format of output, default is 'epub'
+    -o filename    Set filename for the resulting eBook, default is 'eBook'
+
+  Options for Individualization:
+    -t title       Title of the book (meta information)
+    -a author      Author of the book (meta information)
+    -c path        Image for a cover of the book
+    -s html-string Primitive HTML for a cover of the book
+    -e encoding    Encoding                                TODO
 
     [file|dir]      Multiple filenames or directories containing POD
 
     NOTICE: write the options (t,o) before the list of POD files or dirs
 
-  Examples:
-    programname -t mobi file1 file2
-    programname -t epub dir1 dir2
+  Short Examples:
+    programname -f mobi file1 file2
+    programname -f epub dir1 dir2
     programname file*
+
+  Full Example:
+    programname -f mobi \\
+                -c ./cover.jpg \\
+                -s '<h1>The Camel</h1><p>The Coding Camle Guide</p>' \\
+                -t 'The Camel' \\
+                -a 'Boris Daeppen' \\
+                -o 'the_camle.mobi' \\
+                ./*.pod
 
 END
 
 my %opts;
-getopts('ht:o:', \%opts);
+getopts('hf:o:c:s:t:a:', \%opts);
 
 if (exists $opts{h}) {
     print $help_msg;
     exit 0;
 }
 
-if (exists $opts{t}) {
-    if ($opts{t} eq 'mobi') {
+if (exists $opts{f}) {
+    if ($opts{f} eq 'mobi') {
         $out_format = 'Mobi';
         $out_file  .= '.mobi';
     }
-    elsif ($opts{t} eq 'epub') {
+    elsif ($opts{f} eq 'epub') {
         $out_format = 'EPub';
         $out_file  .= '.epub';
     }
     else {
-        die "ERROR: type '$opts{t}' is not supported\n";
+        die "ERROR: type '$opts{f}' is not supported\n";
     }
 }
 else {
     # choose default
     $out_format = 'EPub';
+    $out_file  .= '.mobi';
     print "Choose default type: '$out_format'\n";
 }
 
@@ -89,7 +106,7 @@ print "Input is ok. Starting EPublisher\n\n";
 use EPublisher;
 use EPublisher::Target::Plugin::Mobi;
 
-my $publisher = EPublisher->new(
+my %config = (
     config => { FromCMD => { source => \@in_files,
                           target => { type => $out_format,
                                       output => $out_file
@@ -100,6 +117,28 @@ my $publisher = EPublisher->new(
         print "@_\n";
     },  
 );
+
+if (exists $opts{c}) {
+    $config{config}{FromCMD}{target}{imgcover} = $opts{c};
+}
+
+if (exists $opts{s}) {
+    $config{config}{FromCMD}{target}{htmcover} = $opts{s};
+}
+
+if (exists $opts{t}) {
+    $config{config}{FromCMD}{target}{title} = $opts{t};
+}
+
+if (exists $opts{a}) {
+    $config{config}{FromCMD}{target}{author} = $opts{a};
+}
+
+print "The option-hash I pass to EPublisher:\n";
+use Data::Dumper;
+print Dumper(\%config);
+
+my $publisher = EPublisher->new( %config );
 
 $publisher->run( [ 'FromCMD' ] );
 
